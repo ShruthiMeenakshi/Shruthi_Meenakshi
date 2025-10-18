d6678909b2860463292afaeef4a4f4ee912a17ee
ShruthiMeenakshi <shruthikumar542@gmail.com>
2025-10-18 10:17:24 +0530

    Remove duplicate code in navjs.js for cleaner structure

--- portfolio/Program/navjs.js
+++ portfolio/Program/navjs.js
@@ -1,15 +1,7 @@
-
 const bx = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
 bx.addEventListener('click', () => {
     navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
 });
-=======
-const bx = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
 
-bx.addEventListener('click', () => {
-    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
-});
->>>>>>> eb03ea3b3e6bf0255c71b0b3afa2554ac3a3210f:portfolio/Program/navjs.js
